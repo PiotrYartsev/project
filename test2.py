893840856e57e0e3242595a14a980c63b8b2065a
PiotrYartsev <piotr_1997@live.se>
2022-04-11 13:35:50 +0200

    stuff

--- test2.py
+++ test2.py
@@ -46,12 +46,14 @@ def list_rse():
 
 
 def files_from_datasets(datasets, rses):
+    datasets=list(set(datasets))
     if comments==True:
         print("\nGet a list of all the files in a dataset.")
     
     datasets_rse={}
     for rse in rses:
         list_of_files=[]
+        number_of_files_in_dataset={}
         if limit==0:
             for n in tqdm(range(len(datasets)), disable=tqmdis):
                 dataset=datasets[n]
@@ -59,8 +61,8 @@ def files_from_datasets(datasets, rses):
                     pass
                 else:
                     L=(os.popen("rucio list-file-replicas --rses {} {}".format(rse,dataset)).read()).split('\n')
-                    [file1+dataset for file1 in L]
-                    #print(len(L))
+                    L=[file for file in L if rse in file]
+                    number_of_files_in_dataset[dataset]=len(L)
                     list_of_files.extend(L)
         else:
             if comments==True:
@@ -71,12 +73,13 @@ def files_from_datasets(datasets, rses):
                     pass
                 else:
                     L=(os.popen("rucio list-file-replicas --rses {} {}".format(rse,dataset)).read()).split('\n')
-                    [file1+dataset for file1 in L]
-                    #print(len(L))
+                    L=[file for file in L if rse in file]
+                    number_of_files_in_dataset[dataset]=len(L)
                     list_of_files.extend(L)
+        number_of_files_in_dataset = dict( [(k,v) for k,v in number_of_files_in_dataset.items() if not v==0])
 
-        new_new_list=[file.replace(" ","").split('|') for file in list_of_files]
-        new_new_list=[file for file in new_new_list]
+        new_new_list=[file.replace(" ","").split("|") for file in list_of_files]
+        new_new_list=[file[2:] for file in new_new_list]
         datasets_rse[rse]=new_new_list
         
         if comments==True:
@@ -85,24 +88,20 @@ def files_from_datasets(datasets, rses):
             print(len(datasets_rse[rse]))
 
     datasets_rse = dict( [(k,v) for k,v in datasets_rse.items() if len(v)>0])
-    print(datasets_rse[rse][:10])
-    return(datasets_rse)
+    return(datasets_rse, number_of_files_in_dataset)
         
 def clean_up_datasets_rse(datasets_rse):
     for rse in datasets_rse:
         if comments==True:
             print("\nCleaning up data about files at {}".format(rse))
+
         dataset_list=datasets_rse[rse]
+        #print(dataset_list[10:])
         for n in tqdm(range(len(dataset_list)), disable=tqmdis):
             dataset=dataset_list[n]
-            print(dataset)
-            print(dataset[0])
-            print(dataset[1])
-            print(dataset[4])
-            break
             dataset[3]=dataset[3].replace(dataset[0],"")
             dataset[3]=dataset[3].replace("{}:file://".format(rse),"")
-    return(datasets_rse)
+    return (datasets_rse)
     
 
 #For a provided file file2 and directory dir2 it will return the adler32 shecksum in hexadecimal
@@ -132,7 +131,7 @@ def get_adler32_checksum(file2):
 
 
 #compares the adler34 checksum for the files we matched between storage and rucio. Outputs the rucio entries without a match, the checksum of the file in storage, the checksum as reported by rucio.
-def compere_checksum(datasets_rse):
+def compere_checksum(datasets_rse, number_of_files_in_dataset):
     now = datetime.now()
     now=str(now)
     now=now.replace(" ","_")
@@ -147,6 +146,7 @@ def compere_checksum(datasets_rse):
     not_found_addres=save_dir+"/"+"files_missing_storage.txt"
     found_addres=save_dir+"/"+"files_found_storage.txt"
     missig_in_rucio_addres=save_dir+"/"+"files_missing_rucio.txt"
+    datasets_addres=save_dir+"/"+"datasets_and_numbers.txt"
     for rse in datasets_rse:
         directory_list=[]
         files_found={}
@@ -180,10 +180,10 @@ def compere_checksum(datasets_rse):
                     files_found[rse].append(file)
             elif checksum_dec==None:
                 if rse in files_not_found:
-                    files_not_found[rse].append(found_not_found_str) 
+                    files_not_found[rse].append(not_found_str) 
                 else:   
                     files_not_found[rse]=[]
-                    files_not_found[rse].append(found_not_found_str) 
+                    files_not_found[rse].append(not_found_str) 
             else:
                 if rse in files_found:
                     files_found[rse].append(file) 
@@ -204,7 +204,8 @@ def compere_checksum(datasets_rse):
         number_failed_files=0
         number_sucesfull_files=0
         for rse in files_not_found:
-            number_failed_files=number_failed_files+len(files_not_found[rse])
+            for k in files_not_found[rse]:
+                number_failed_files=number_failed_files+len(files_not_found[rse])
         
         for rse in files_found:
             number_sucesfull_files=number_sucesfull_files+len(files_found[rse])
@@ -216,26 +217,38 @@ def compere_checksum(datasets_rse):
             print("\nFor the rse {} we found that {} files were missing out of {}.".format(rse, number_failed_files, len(datasets)))
             if checksum==True:
                 print("We found {} corrupted files out of {}".format(integ,(len(datasets))))
+
         not_found= open("{}".format(not_found_addres),"w+")
-        found= open("{}".format(found_addres),"w+")
+
+       
+
+        #found= open("{}".format(found_addres),"w+")
+         
         files_not_in_rucio=[]
         for n in files_not_found:
             not_found.write(str(files_not_found[n]))
-        for n in files_found:
-            found.write(str(files_found[n]))
+
+        #for n in files_found:
+        #  found.write(str(files_found[n]))
+        
+        print("\nFor each storage location find what files in storage is not registered in Rucio.")
         for n in tqdm(range(len(directory_list)), disable=tqmdis):
             directory=directory_list[n]
             files_not_in_rucio.extend(list(os.popen("ls {}".format(directory))))
             #[file.replace("\n","") for file in files_not_in_rucio]
         not_found.close()
-        found.close()
+        #found.close()
 
         not_in_rucio=open("{}".format(missig_in_rucio_addres),"w+")
 
         missig_in_rucio=set(files_not_in_rucio)-set(files_found[rse])
         missig_in_rucio=list(missig_in_rucio)
-
+        print("\nWe found {} missing files in storage that are registered in rucio. PS: Unless you searched all scopes this does not mean much.".format(len(missig_in_rucio)))
         for file in missig_in_rucio:
             not_in_rucio.write(file)
         not_in_rucio.close()
-        
+    datasets= open("{}".format(datasets_addres),"w+")       
+    
+    datasets.write(str(number_of_files_in_dataset))
+
+    datasets.close()
