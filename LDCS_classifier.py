126e6a4007430f2810c05b6fc65efba4469f4ebd
PiotrYartsev <piotr_1997@live.se>
2022-04-16 00:41:37 +0200

    nb

--- LDCS_classifier.py
+++ LDCS_classifier.py
@@ -1,9 +1,111 @@
-
-
-
+import os
+from zlib import adler32
+from datetime import datetime
+from tqdm import *
+from run_test import *
 from numpy import size
 
+if not os.path.exists('classifier'):
+        os.makedirs('classifier')
+if not os.path.exists('classifier/checked.txt'):
+        os.mknod('classifier/checked.txt')
 
+output_list=next(os.walk('output'))[1]
+
+checked=[]
+
+checked_file=open("{}".format('classifier/checked.txt'),"r")
+checked_file_lines=checked_file.readlines()
+for line in checked_file_lines:
+    checked.append(line)
+checked_file.close()
+
+
+output_list_to_check=[file for file in output_list if file not in checked]
+#print(output_list_to_check)
+    
+
+for output in output_list_to_check:
+    files_in_output=next(os.walk('output/{}'.format(output)))[2]
+    #print(files_in_output)
+    if "files_missing_storage.txt" in files_in_output:
+        #print("yes")
+        files_missing_storage=open("output/{}/files_missing_storage.txt".format(output),"r")
+
+        files_missing_storage_lines=files_missing_storage.readlines()
+        #print(files_missing_storage_lines)
+        files_missing_storage_list=[]
+
+        for line in files_missing_storage:
+            #print(line)
+            files_missing_storage_list.append(line.split(","))
+        files_missing_storage.close()
+
+        datasets_and_numbers=open("output/{}/datasets_and_numbers.txt".format(output),"r")
+        datasets_and_numbers_list=[]
+
+        for line in datasets_and_numbers:
+            #print(line)
+            datasets_and_numbers_list.append(line.split(","))
+        datasets_and_numbers.close()
+
+        files_problem_batch=[]
+        files_really_problem_batch=[]
+        files_missing_batch=[]
+
+        for file in datasets_and_numbers_list:
+            batch=file[0]
+            number=int(file[1])
+            n=0
+            for stuff in files_missing_storage_lines:
+                if batch in stuff:
+                    n=n+1
+            if n/number>0:
+                if n/number>0.1 and n/number<0.2:
+                    files_problem_batch.append(batch)
+                elif n/number>0.2 and not n/number==1:
+                    files_really_problem_batch.append(batch)
+                elif n/number==1:
+                    files_missing_batch.append(batch)    
+
+        files_problem_batch_list=[]
+        for file in files_problem_batch:
+            for stuff in files_missing_storage_lines:
+                if file in stuff:
+                    files_problem_batch_list.append(stuff)
+
+        files_really_problem_batch_list=[]
+        for file in files_really_problem_batch:
+            for stuff in files_missing_storage_lines:
+                if file in stuff:
+                    files_really_problem_batch_list.append(stuff)
+
+        files_missing_batch_list=[]
+        for file in files_missing_batch:
+            for stuff in files_missing_storage_lines:
+                if file in stuff:
+                    files_missing_batch_list.append(stuff)
+
+        if len(files_problem_batch_list)>0:
+            for file_info in files_problem_batch_list:
+                file=file_info.split(",")
+                files_problem_batch_txt=open("output/{}/files_problem_batch.txt".format(output),"w+")
+                files_problem_batch_txt.write()
+                
+
+
+        print(len(files_problem_batch_list))
+        print(len(files_really_problem_batch_list))
+        print(len(files_missing_batch_list))
+
+        
+           
+
+
+
+        
+
+"""
 while open("missing") as f:
     for line in f:
         listOfLine=line.split(",")
@@ -61,3 +163,4 @@ for missing_file in list_of_missing_files:
     same_time=simulation_run[]
     if many-files-created at around a same_time:
         print("Probablu a problem with the run {} that was at {}".format(simulation_run, same_time))
+"""
