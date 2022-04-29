28a6715412a72797ce3110ed91bd543ebf27c334
PiotrYartsev <piotr_1997@live.se>
2022-04-29 17:18:20 +0200

    classification now works remotly

--- test_file_found.py
+++ test_file_found.py
@@ -3,12 +3,12 @@ from zlib import adler32
 from datetime import datetime
 
 
-a=open("output/All_LUND_2022-04-28_20:59:54.348727/files_found_storage.txt", "r")
+a=open("classifier/All_LUND_GRIDFTP_2022-04-27_14:30:24.981447/files_missing_rucio/currupted_duplicate.txt", "r")
 
 lines=a.readlines()
 
 print(len(lines))
 
-lines=list(set(lines))
+lines=[line for line in lines if not line=="\n"]
 
 print(len(lines))
