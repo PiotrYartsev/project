b01ced74fec95a3a456e6b5c4d3a471250d5cac9
Piotr Yartsev <55767612+PiotrYartsev@users.noreply.github.com>
2023-06-09 21:27:16 +0200

    Update test2_copy.py

--- test2_copy.py
+++ test2_copy.py
@@ -1,18 +1,42 @@
-
-from DDS_comparison_tool import *
 from rucio.client import Client
+
 rucioclient = Client()
+
+import json
+
 #using the rucio API to get the list of scopes
 def list_scopes():
     scopes=rucioclient.list_scopes()
     return(scopes)
-#using the rucio API to get the list of datasets
-def get_all_datasets(scopes):
-    All_datasets=[]
-    #Iterate over all scopes, run function get_datasets_from_scope from scope
-    for scope in scopes:
-        datasets_scope=rucioclient.list_files(scope)
-    return datasets_scope
-    
+
+def list_rses():
+    rses=rucioclient.list_rses()
+    rses = json.loads(rses)
+    return(rses)
+
+#using the rucio API to get all the files in a rse
+def list_files_rse(rse_name):
+    try:
+        files=rucioclient.list_replicas([rse_name])
+        return(files)
+    except Exception as e:
+        print(f"Error: {e}")
+        return []
+
 scopes=list_scopes()
-datasets=get_all_datasets(scopes)
+#print(scopes)
+
+rses=list_rses()
+print(rses)
+
+rses=list(rses)
+
+# prompt the user to enter the RSE name
+rse_name = input("Enter the RSE name: ")
+
+files=list_files_rse(rse_name)
+
+if files:
+    print(files)
+else:
+    print(f"No files found in RSE {rse_name}")
