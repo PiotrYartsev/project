89ed61e502395c7f26150c28e6e090d135bf93fb
Piotr Yartsev <55767612+PiotrYartsev@users.noreply.github.com>
2023-06-18 12:20:15 +0200

    This is some great coding right here :smile:  Added generation of the documents for reading the direcotries. Fixed some other stuff like minor bugs

---
+++ DDS-V2/Rucio_functions.py
@@ -0,0 +1,40 @@
+from rucio.client import Client
+
+rucioclient = Client()
+
+#using the rucio API to get the list of scopes
+def list_scopes():
+    scopes=rucioclient.list_scopes()
+    return(scopes)
+
+#using the rucio API to get the list of rses
+def list_rses():
+    rses=rucioclient.list_rses()
+    return(rses)
+
+#using the rucio API to get all the files in a rdataset
+def list_files_dataset(scope, name):
+    files=rucioclient.list_files(scope=scope, name=name, long=True)
+    return(files)
+
+#use the rucio API to get the list of replicas for a file
+def list_replicas(scope, name):
+    replicas=rucioclient.list_replicas(scope=scope, name=name)
+    return(replicas)
+
+#use the rucio API to get the statistics of rse usage
+def get_rse_usage(rse):
+    rse_info=rucioclient.get_rse_usage(rse)
+    return(rse_info)
+
+#use the rucio API to get metadata in bulk
+def get_metadata(scope, name):
+    metadata=rucioclient.get_metadata(scope, name)
+
+
+#use the rucio API to get the list of replicas fo a file, which gives us the where the file is stored 
+def list_replicas(scope, name):
+    replicas=rucioclient.list_replicas(dids=[{'scope':scope, 'name':name}])
+    return(replicas)
+
+
---
+++ DDS-V2/generate_text_rse_docs.py
@@ -0,0 +1,29 @@
+# Import libraries
+import sqlite3 as sl
+# Library for creating directories
+import os
+# Library for getting the current date and time
+import datetime
+import sys
+
+# Connect to the SQLite database
+directories_database = sl.connect('directories_and_dataset_for_RSE.db')
+
+# For each RSE in the table
+for table in directories_database.execute("SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'datasets'"):
+    # If directory with name rse does not already exist, create it
+    table = table[0]
+    if table not in ["dataset","sqlite_sequence"]:
+        if not os.path.exists(table):
+            # Create directory
+            os.mkdir(table)
+        # Create file in the rse directory and name it the date and time
+        file_name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".txt"
+        # Create file
+        file = open(table + "/" + file_name,"w")
+        # Write the directories from the SQLite database to the file
+        for row in directories_database.execute("SELECT directory FROM " + table).fetchall():
+            row = row[0]
+            file.write(row + "\n")
+        # Close file
+        file.close()
