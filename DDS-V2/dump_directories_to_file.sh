863edcc53a84687d27a438d92ccc5d0d7b6743fe
Piotr Yartsev <55767612+PiotrYartsev@users.noreply.github.com>
2023-06-19 08:59:50 +0200

    added bash script

--- DDS-V2/dump_directories_to_file.sh
+++ DDS-V2/dump_directories_to_file.sh
@@ -1,1 +1,23 @@
-#start the scripst
+#!/bin/bash
+# 
+
+#this srcipt will read a file line by line, as ls the directory with the name of the line
+#and dump the result to a file with the name of the line
+
+#the file to read from
+#2023-06-18_10-20-30.txt
+
+#the directory to dump to
+#2023-06-18_10-20-30
+
+#start the loop
+while read line
+do
+    #ls the directory
+    ls $line > $line.txt
+done < $1
+
+
+
+
+
