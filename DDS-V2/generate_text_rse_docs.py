3a6285fe70ecc553a44c6fd39b1cfaee0d8ab191
Piotr Yartsev <55767612+PiotrYartsev@users.noreply.github.com>
2023-06-19 18:43:18 +0200

    something went a bit wrong, delting the data and regenarating to be sure

--- DDS-V2/generate_text_rse_docs.py
+++ DDS-V2/generate_text_rse_docs.py
@@ -9,18 +9,22 @@ import sys
 # Connect to the SQLite database
 directories_database = sl.connect('directories_and_dataset_for_RSE.db')
 
+#if directory RSE does not exist, create it
+if not os.path.exists("RSE"):
+    # Create directory
+    os.mkdir("RSE")
 # For each RSE in the table
 for table in directories_database.execute("SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'datasets'"):
     # If directory with name rse does not already exist, create it
     table = table[0]
     if table not in ["dataset","sqlite_sequence"]:
-        if not os.path.exists(table):
+        if not os.path.exists("RSE/"+table):
             # Create directory
-            os.mkdir(table)
+            os.mkdir("RSE/"+table)
         # Create file in the rse directory and name it the date and time
         file_name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".txt"
         # Create file
-        file = open(table + "/" + file_name,"w")
+        file = open("RSE/"+table + "/" + file_name,"w")
         # Write the directories from the SQLite database to the file
         for row in directories_database.execute("SELECT directory FROM " + table).fetchall():
             row = row[0]
