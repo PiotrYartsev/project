be0d4a34195da0ed6055f51d2559328383b98d25
Piotr Yartsev <55767612+PiotrYartsev@users.noreply.github.com>
2023-06-20 12:08:51 +0200

    made some changes

--- DDS-V2/read_rse_output_and_compare_to_rucio.py
+++ DDS-V2/read_rse_output_and_compare_to_rucio.py
@@ -7,10 +7,13 @@ import datetime
 
 
 #for subdirectory in RSE,not files, which is located in the same directory as this script
-for subdirectory in os.listdir(os.getcwd()+"/RSE"):
-    print(subdirectory)
+for rse in os.listdir(os.getcwd()+"/RSE"):
+    print(rse)
     #for output directory exiist in subdirectory
-    if os.path.isdir(os.getcwd()+"/RSE/"+subdirectory+"/output"):
+    if os.path.isdir(os.getcwd()+"/RSE/"+rse+"/output"):
         #for file in output directory
-        for file in os.listdir(os.getcwd()+"/RSE/"+subdirectory+"/output"):
-            print(file)
+        for file in os.listdir(os.getcwd()+"/RSE/"+rse+"/output"):
+            #in each file, read the first line
+            with open(os.getcwd()+"/RSE/"+rse+"/output/"+file) as f:
+                first_line = f.readline()
+                print(first_line)
