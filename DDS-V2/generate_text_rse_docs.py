3ba51d5f508b791d2c695af0e84aa36a26367ca8
Piotr Yartsev <55767612+PiotrYartsev@users.noreply.github.com>
2023-07-06 15:32:20 +0200

    working on verification

--- DDS-V2/generate_text_rse_docs.py
+++ DDS-V2/generate_text_rse_docs.py
@@ -13,13 +13,22 @@ rucio_database = sl.connect('Rucio_data_LUND_GRIDFTP.db')
 #get the datasets
 datasets = rucio_database.execute("SELECT * FROM dataset").fetchall()
 
-for rse in os.listdir("RSE"):
-    if os.path.isdir("RSE/"+rse):
-        #move all files there to the archive folder
-        os.system("mv RSE/"+rse+"/* archives")
-
 #date day-month-year
 date=datetime.datetime.now().strftime("%d-%m-%Y")
+for rse in os.listdir("RSE"):
+    print(rse)
+    if os.path.isdir("RSE/"+rse):
+        #if anything is in the directory move to archive
+        if os.listdir("RSE/"+rse):
+            #create archives/date
+            if not os.path.exists("archives/"+date):
+                os.mkdir("archives/"+date)
+            #move the files in the directory to the archive
+
+            os.system(f"mv RSE/{rse} archives/"+date)
+
+
+
 
 for dataset in datasets:
     #print(dataset)
@@ -27,9 +36,9 @@ for dataset in datasets:
     directory = ast.literal_eval(directory)
     for list in directory:
         rse=list[0]
-        print(rse)
+        #print(rse)
         directory=list[1]
-        print(directory)
+        #print(directory)
         if not os.path.exists("RSE/"+rse):
             # Create directory
             os.mkdir("RSE/"+rse)
@@ -40,4 +49,15 @@ for dataset in datasets:
         #close the file
         file.close()
         
-        
+for dataset in rucio_database.execute("Select name from dataset").fetchall():
+    #retrive the directory
+    directory = rucio_database.execute("SELECT directory FROM dataset WHERE name = ?", dataset).fetchall()[0][0]
+    #print(directory)
+    #transofrm from str to list
+    directory = ast.literal_eval(directory)
+    #print(directory)
+    directory_write=[a[1] for a in directory]
+    #print(directory_write)
+    #overwrite the directory
+    rucio_database.execute("UPDATE dataset SET directory = ? WHERE name = ?", (str(directory_write), dataset[0]))
+    rucio_database.commit()
