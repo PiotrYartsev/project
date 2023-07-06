7d679cd09d437f4367acde41fbe6b4ede18c0db1
Piotr Yartsev <55767612+PiotrYartsev@users.noreply.github.com>
2023-07-06 15:32:20 +0200

    working on verification

--- DDS-V2/verify.py
+++ DDS-V2/verify.py
@@ -10,23 +10,41 @@ def main():
         print(rse)
         if os.path.isdir("RSE/"+rse+"/Dark_data"):
             missing_from_rucio(rse)
-            break
+            create_missing_from_storage(rse)
             
 
-
 def missing_from_rucio(rse):
-    missing_from_rucio_database = sl.connect("RSE/"+rse+"/Dark_data/"+'missing_from_rucio.db')
-    for table in missing_from_rucio_database.execute("SELECT name FROM sqlite_master WHERE type='table';"):
-        print(table[0])
-        get_files_missing_from_rucio = missing_from_rucio_database.execute("SELECT scope,name FROM "+table[0]).fetchall()
-        #make a list of list where each list is 999 files long
-        splitlist=[get_files_missing_from_rucio[i:i + 999] for i in range(0, len(get_files_missing_from_rucio), 999)]
-    
-        delete_list=[]
-        for list in splitlist:
-            value=(check_files_exist(list))
-            delete_list.extend(value)
-        print(delete_list)
+    #check if the database exists
+    if not os.path.isfile("RSE/"+rse+"/Dark_data/"+'missing_from_rucio.db'):
+        missing_from_rucio_database = sl.connect("RSE/"+rse+"/Dark_data/"+'missing_from_rucio.db')
+        for table in missing_from_rucio_database.execute("SELECT name FROM sqlite_master WHERE type='table';"):
+            print(table[0])
+            get_files_missing_from_rucio = missing_from_rucio_database.execute("SELECT scope,name FROM "+table[0]).fetchall()
+            #make a list of list where each list is 999 files long
+            splitlist=[get_files_missing_from_rucio[i:i + 999] for i in range(0, len(get_files_missing_from_rucio), 999)]
+        
+            delete_list=[]
+            for list in splitlist:
+                value=(check_files_exist(list))
+                delete_list.extend(value)
+            print(delete_list)
+            #delete the files from the database
+            for file in delete_list:
+                missing_from_rucio_database.execute("DELETE FROM "+table[0]+" WHERE scope=? AND name=?",file)
+            missing_from_rucio_database.commit()
+            #if the table is empty delete it
+            if len(missing_from_rucio_database.execute("SELECT * FROM "+table[0]).fetchall())==0:
+                missing_from_rucio_database.execute("DROP TABLE "+table[0])
+                missing_from_rucio_database.commit()
+        missing_from_rucio_database.close()
+
+
+def create_missing_from_storage(rse):
+    #check if the database exists
+    if not os.path.isfile("RSE/"+rse+"/Dark_data/"+'missing_from_storage.db'):
+
+
+
 
 
 if __name__ == "__main__":
