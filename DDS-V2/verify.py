8b075e25a30df2652cbacdd0426bf79729cb0c59
Piotr Yartsev <55767612+PiotrYartsev@users.noreply.github.com>
2023-07-06 21:34:55 +0200

    w

--- DDS-V2/verify.py
+++ DDS-V2/verify.py
@@ -15,19 +15,21 @@ def main():
 
 def missing_from_rucio(rse):
     #check if the database exists
-    if not os.path.isfile("RSE/"+rse+"/Dark_data/"+'missing_from_rucio.db'):
+    if os.path.isfile("RSE/"+rse+"/Dark_data/"+'missing_from_rucio.db'):
         missing_from_rucio_database = sl.connect("RSE/"+rse+"/Dark_data/"+'missing_from_rucio.db')
+        
         for table in missing_from_rucio_database.execute("SELECT name FROM sqlite_master WHERE type='table';"):
             print(table[0])
             get_files_missing_from_rucio = missing_from_rucio_database.execute("SELECT scope,name FROM "+table[0]).fetchall()
+            print(len(get_files_missing_from_rucio))
             #make a list of list where each list is 999 files long
             splitlist=[get_files_missing_from_rucio[i:i + 999] for i in range(0, len(get_files_missing_from_rucio), 999)]
         
             delete_list=[]
-            for list in splitlist:
+            for list in tqdm(splitlist):
                 value=(check_files_exist(list))
                 delete_list.extend(value)
-            print(delete_list)
+            print(len(delete_list))
             #delete the files from the database
             for file in delete_list:
                 missing_from_rucio_database.execute("DELETE FROM "+table[0]+" WHERE scope=? AND name=?",file)
@@ -42,7 +44,7 @@ def missing_from_rucio(rse):
 def create_missing_from_storage(rse):
     #check if the database exists
     if not os.path.isfile("RSE/"+rse+"/Dark_data/"+'missing_from_storage.db'):
-
+        pass
 
 
 
